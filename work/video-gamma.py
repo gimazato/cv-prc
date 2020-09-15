5fb382584b18bcc6387576484db30b9c3e88e676
gimazato <example@hoge.com>
2020-09-15 23:36:59 +0900

    平滑化関数を追加。input, output変数追加。コメント修正

--- work/video-gamma.py
+++ work/video-gamma.py
@@ -7,36 +7,43 @@ def create_gamma_img(gamma, img):
     gamma_cvt_lut[i][0] = 255*(float(i)/255)**(1.0/gamma)
   return cv2.LUT(img, gamma_cvt_lut)
 
+def create_equalizeHist_img(img):
+  return cv2.equalizeHist(img)
+
 
 print("---start---")
 
-#動画ファイルを読み込む
-video = cv2.VideoCapture("./videos/street.mp4")
+# 動画ファイルを読み込む
+input_filename = "./videos/street.mp4"
+video = cv2.VideoCapture(input_filename)
 
 # 幅と高さを取得
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 size = (width, height)
 
-#総フレーム数を取得
+# 総フレーム数を取得
 frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-#フレームレート(1フレームの時間単位はミリ秒)の取得
+# フレームレート(単位はミリ秒)の取得
 frame_rate = int(video.get(cv2.CAP_PROP_FPS))
 
 # 保存用
+output_filename = "./videos/street-equalized.mp4"
 fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-writer = cv2.VideoWriter('./videos/street-gamma.mp4', fmt, frame_rate, size)
+writer = cv2.VideoWriter(output_filename, fmt, frame_rate, size)
 
 for i in range(frame_count):
     ret, frame = video.read()
-    ### ここに加工処理などを記述する ###
-    frame = create_gamma_img(2, frame)
-    ####################################
+    ### ここに加工処理などを記述する 
+    #frame = create_gamma_img(2, frame)
+    frame = create_equalizeHist_img(frame)
+
+    
     writer.write(frame)
 
     # 現在読んでいるフレームを描画
     # 処理結果見えなくていいから軽量化したい場合はここをコメントアウト
-    # dockerだとx-winndow? 共有設定が何かしら必要。要調査
+    # dockerだとrun実行時に、x-winndow? 共有設定が何かしら必要？。要調査
     #cv2.imshow("frame", frame)
 
     # qを押せば処理を中止できるようにしておく
